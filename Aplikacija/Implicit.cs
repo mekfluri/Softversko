ddcc7adf2e399ddfeac9c56618df93bfa79f5d67
Urosh <urosh_h@protonmail.com>
2023-04-24 18:28:48 +0200

    implicit usings

---
+++ Aplikacija/Implicit.cs
@@ -0,0 +1,18 @@
+global using JWT;
+global using System.Text;
+global using System.ComponentModel;
+global using Microsoft.IdentityModel.Tokens;
+global using System.IdentityModel.Tokens.Jwt;
+global using System.Security.Claims;
+global using Microsoft.AspNetCore.Identity;
+global using System.Security.Cryptography;
+global using System.Text.Json.Serialization;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Design;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.EntityFrameworkCore.SqlServer;
+global using System.ComponentModel.DataAnnotations.Schema;
+
+global using Models;
+global using Helpers;
