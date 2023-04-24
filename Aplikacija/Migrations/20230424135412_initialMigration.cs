6d111551aa12fe63802dfcbbd4565abb6cc05bc6
Urosh <urosh_h@protonmail.com>
2023-04-24 18:30:42 +0200

    initial migrations

---
+++ Aplikacija/Migrations/20230424135412_initialMigration.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aplikacija.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Predmeti",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modul = table.Column<int>(type: "int", nullable: false),
+                    Semestar = table.Column<int>(type: "int", nullable: false),
+                    ESPB = table.Column<int>(type: "int", nullable: false),
+                    Opis = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Predmeti", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Student",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Modul = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Semestar = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Student", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Ocene",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DostupnostMaterijala = table.Column<int>(type: "int", nullable: false),
+                    AngazovanjeProfesora = table.Column<int>(type: "int", nullable: false),
+                    LaboratorijskeVezbe = table.Column<int>(type: "int", nullable: false),
+                    TezinaPredmeta = table.Column<int>(type: "int", nullable: false),
+                    PrakticnoZnanje = table.Column<int>(type: "int", nullable: false),
+                    PredmetId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Ocene", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Ocene_Predmeti_PredmetId",
+                        column: x => x.PredmetId,
+                        principalTable: "Predmeti",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Tagovi",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PredmetId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tagovi", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Tagovi_Predmeti_PredmetId",
+                        column: x => x.PredmetId,
+                        principalTable: "Predmeti",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kalendari",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kalendari", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Kalendari_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Komentari",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    PredmetId = table.Column<int>(type: "int", nullable: false),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Komentari", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Komentari_Predmeti_PredmetId",
+                        column: x => x.PredmetId,
+                        principalTable: "Predmeti",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Komentari_Student_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Student",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Preference",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TagId = table.Column<int>(type: "int", nullable: false),
+                    OcenaId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Preference", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Preference_Ocene_OcenaId",
+                        column: x => x.OcenaId,
+                        principalTable: "Ocene",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Preference_Tagovi_TagId",
+                        column: x => x.TagId,
+                        principalTable: "Tagovi",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Datum",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Poruka = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OznacenDatum = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    KalendarId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Datum", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Datum_Kalendari_KalendarId",
+                        column: x => x.KalendarId,
+                        principalTable: "Kalendari",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Datum_KalendarId",
+                table: "Datum",
+                column: "KalendarId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Kalendari_StudentId",
+                table: "Kalendari",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Komentari_PredmetId",
+                table: "Komentari",
+                column: "PredmetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Komentari_StudentId",
+                table: "Komentari",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Ocene_PredmetId",
+                table: "Ocene",
+                column: "PredmetId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preference_OcenaId",
+                table: "Preference",
+                column: "OcenaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Preference_TagId",
+                table: "Preference",
+                column: "TagId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tagovi_PredmetId",
+                table: "Tagovi",
+                column: "PredmetId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Datum");
+
+            migrationBuilder.DropTable(
+                name: "Komentari");
+
+            migrationBuilder.DropTable(
+                name: "Preference");
+
+            migrationBuilder.DropTable(
+                name: "Kalendari");
+
+            migrationBuilder.DropTable(
+                name: "Ocene");
+
+            migrationBuilder.DropTable(
+                name: "Tagovi");
+
+            migrationBuilder.DropTable(
+                name: "Student");
+
+            migrationBuilder.DropTable(
+                name: "Predmeti");
+        }
+    }
+}
---
+++ Aplikacija/Migrations/20230424145948_v1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aplikacija.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Student",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Student");
+        }
+    }
+}
