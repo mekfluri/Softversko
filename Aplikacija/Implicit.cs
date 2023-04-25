58ccdcbf3c620fd6ac062263adcfef635b6bc7df
Urosh <urosh_h@protonmail.com>
2023-04-25 15:48:25 +0200

    new implicit using

--- Aplikacija/Implicit.cs
+++ Aplikacija/Implicit.cs
@@ -2,6 +2,7 @@ global using JWT;
 global using System.Text;
 global using System.ComponentModel;
 global using Microsoft.IdentityModel.Tokens;
+global using Microsoft.AspNetCore.Authorization;
 global using System.IdentityModel.Tokens.Jwt;
 global using System.Security.Claims;
 global using Microsoft.AspNetCore.Identity;
