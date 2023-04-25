23e42c676ddc8d00e46576e2d3da270d9b6604cc
Urosh <urosh_h@protonmail.com>
2023-04-25 15:48:14 +0200

    Modified claims for auth

--- Aplikacija/Helpers/AuthService.cs
+++ Aplikacija/Helpers/AuthService.cs
@@ -12,7 +12,10 @@ public class AuthService {
         var key = _config["Jwt:Key"]!;
         var token = CreateToken(
             CreateClaims(userInfo),
-            new SigningCredentials(new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)), SecurityAlgorithms.HmacSha256),
+            new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                SecurityAlgorithms.HmacSha256
+            ),
             expiration
         );
         return tokenHandler.WriteToken(token);
@@ -20,15 +23,17 @@ public class AuthService {
 
     private List<Claim> CreateClaims(LoginModel userInfo) {
         var claims = new List<Claim>{
-            new Claim(ClaimTypes.Email, userInfo.Email),
+            new Claim(JwtRegisteredClaimNames.Email, userInfo.Email),
+            new Claim(JwtRegisteredClaimNames.Aud, _config["Jwt:Audience"]!),
+            new Claim(JwtRegisteredClaimNames.Iss, _config["Jwt:Issuer"]!)
         };
         return claims;
     }
 
     private JwtSecurityToken CreateToken(List<Claim> claims, SigningCredentials creds, DateTime expiration){
         return new JwtSecurityToken(
-            null,
-            null,
+            issuer: _config["Jwt:Issuer"],
+            audience: _config["Jwt:Audience"],
             claims,
             expires: expiration,
             signingCredentials: creds
