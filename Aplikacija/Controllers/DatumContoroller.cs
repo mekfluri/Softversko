e98fe22aaf1648689de8547473d8101f2076618f
Jovana <suslic.jovana@elfak.rs>
2023-04-27 13:10:07 +0200

    Dodali smo kontroler za datum

---
+++ Aplikacija/Controllers/DatumContoroller.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aplikacija.Controllers;
+
+[ApiController]
+[Route("/datum")]
+public class DatumContorller : ControllerBase
+{
+      public IzaberryMeDbContext Context { get; set; }
+
+    public DatumContorller(IzaberryMeDbContext context)
+    {
+        Context = context;
+    }
+   
+   [HttpPost("dodajdatum")]
+   public async Task<ActionResult> dodajDatum([FromBody] Datum datum)
+    {
+         try
+        {
+          Context.Datumi.Add(datum);
+          await Context.SaveChangesAsync();
+          return Ok("Dodali smo datum sa id-jem"+datum.Id);
+        }
+        catch(Exception e)
+        {
+          return BadRequest(e.Message);
+        }
+   }
+
+   [HttpDelete("obrisiDatum/{iddatuma}")]
+   public async Task<ActionResult> obrisiDatum(int iddatuma)
+   {
+       var k = await Context.Datumi.FindAsync(iddatuma);
+       
+       if(k != null)
+       {
+         Context.Datumi.Remove(k);
+         await Context.SaveChangesAsync();
+         return Ok("Obrisali smo  datum sa rednim brojem" + iddatuma);
+       }
+       else 
+       {
+        return BadRequest("Ne postoji takav datum");
+       }
+   }
+
+   
+}
