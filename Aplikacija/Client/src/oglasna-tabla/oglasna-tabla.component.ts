5886e4139127a4046d02fe5dbe059f19a261fbb3
Anja Cokanovic <anja.cokanovic@elfak.rs>
2023-04-27 13:26:09 +0200

    oglasna-tabla komponenta

---
+++ Aplikacija/Client/src/oglasna-tabla/oglasna-tabla.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-oglasna-tabla',
+  templateUrl: './oglasna-tabla.component.html',
+  styleUrls: ['./oglasna-tabla.component.css']
+})
+export class OglasnaTablaComponent {
+
+}
