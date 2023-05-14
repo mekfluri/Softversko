d870f624f60f313f07178305f260e901e36df43e
Anja Cokanovic <anja.cokanovic@elfak.rs>
2023-05-14 14:22:47 +0200

    predmeti components

---
+++ Aplikacija/Client/izaberry-client/src/app/components/predmeti/predmeti.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predmeti',
+  templateUrl: './predmeti.component.html',
+  styleUrls: ['./predmeti.component.scss']
+})
+export class PredmetiComponent {
+
+}
