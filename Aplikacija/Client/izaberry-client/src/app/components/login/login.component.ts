e2a9252657505f36ea94e2c6b2eb28d41bee2013
Urosh <urosh_h@protonmail.com>
2023-05-16 21:01:58 +0200

    Login component

--- Aplikacija/Client/izaberry-client/src/app/components/login/login.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import { LoginModel } from 'src/app/models/login.model';
+import { AuthService } from 'src/app/services/auth.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -7,8 +10,37 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
-  constructor(private router: Router){}
+  email: string;
+  password: string;
+
+  constructor(private router: Router, private authService: AuthService, private userService: UserService) {
+    this.email = "";
+    this.password = "";
+  }
+
   redirectToSignup() {
     this.router.navigateByUrl("signup");
+  }
+
+  async loginRequest() {
+    try {
+      let loginCredentials = new LoginModel(this.email, this.password);
+      let token = await this.authService.login(loginCredentials);
+      localStorage.setItem("authToken", token);
+      let user = await this.userService.getUserByToken(token);
+      //redirect na profil
+      this.router.navigate(["profile"], {
+        state: user!
+      });
+    }
+    catch(err: any) {
+      alert((err as Error).message);
+    }
+  }
+  emailKeyUp(event: KeyboardEvent) {
+    this.email = (event.target as HTMLInputElement).value;
+  }
+  passwordKeyUp(event: KeyboardEvent) {
+    this.password = (event.target as HTMLInputElement).value;
   }
 }
