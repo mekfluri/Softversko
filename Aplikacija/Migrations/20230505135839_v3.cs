434c1f1112182250d9c908e502753fed6fa0f21a
Urosh <urosh_h@protonmail.com>
2023-05-16 21:06:24 +0200

    Migrations

---
+++ Aplikacija/Migrations/20230505135839_v3.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aplikacija.Migrations
+{
+    /// <inheritdoc />
+    public partial class v3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Datum_Kalendari_KalendarId",
+                table: "Datum");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Datum",
+                table: "Datum");
+
+            migrationBuilder.RenameTable(
+                name: "Datum",
+                newName: "Datumi");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Datum_KalendarId",
+                table: "Datumi",
+                newName: "IX_Datumi_KalendarId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Modul",
+                table: "Predmeti",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Datumi",
+                table: "Datumi",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Datumi_Kalendari_KalendarId",
+                table: "Datumi",
+                column: "KalendarId",
+                principalTable: "Kalendari",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Datumi_Kalendari_KalendarId",
+                table: "Datumi");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Datumi",
+                table: "Datumi");
+
+            migrationBuilder.RenameTable(
+                name: "Datumi",
+                newName: "Datum");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Datumi_KalendarId",
+                table: "Datum",
+                newName: "IX_Datum_KalendarId");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Modul",
+                table: "Predmeti",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Datum",
+                table: "Datum",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Datum_Kalendari_KalendarId",
+                table: "Datum",
+                column: "KalendarId",
+                principalTable: "Kalendari",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ Aplikacija/Migrations/20230514165957_v4.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aplikacija.Migrations
+{
+    /// <inheritdoc />
+    public partial class v4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Moduli",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Naziv = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Moduli", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Moduli");
+        }
+    }
+}
