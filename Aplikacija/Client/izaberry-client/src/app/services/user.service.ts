a3dbba20849552a6fac13acc4c2895e7a0ef425e
Urosh <urosh_h@protonmail.com>
2023-05-20 15:58:22 +0200

    user-service & auth-service update

--- Aplikacija/Client/izaberry-client/src/app/services/user.service.ts
+++ Aplikacija/Client/izaberry-client/src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Student } from '../models/student.model';
+import { Komentar } from '../models/komentar.model';
 import { environment } from 'src/environments/environment';
 import { firstValueFrom } from 'rxjs';
 
@@ -9,15 +10,20 @@ import { firstValueFrom } from 'rxjs';
 })
 export class UserService {
 
+  user: Student | null = null;
+
   constructor(private http: HttpClient) { }
 
   async getUserByToken(token: string): Promise<Student | null> {
-    let headers = new HttpHeaders()
-      .set("Authorization", `Bearer ${token}`);
-    let user$ = this.http.get<Student>(`${environment.backend}/student`, {
-      headers
-    });
-    let user = await firstValueFrom(user$);
-    return user;
+    let user$ = this.http.get<Student>(`${environment.backend}/student`);
+    this.user = await firstValueFrom(user$);
+    this.user.token = token;
+    return this.user;
+  }
+
+  async getUserComments(): Promise<Komentar[] | null> {
+    let komentari$ = this.http.get<Komentar[]>(`${environment.backend}/komentari/${this.user?.id}`);
+    let komentari = await firstValueFrom(komentari$) ?? null;
+    return komentari;
   }
 }
