433f5ddf91aac313a0acd0f2270cffbf0b29d627
Urosh <urosh_h@protonmail.com>
2023-05-24 15:48:59 +0200

    predmet-preview to oglasna-tabla button

--- Aplikacija/Client/izaberry-client/src/app/components/predmet-preview/predmet-preview.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/predmet-preview/predmet-preview.component.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { Komentar } from 'src/app/models/komentar.model';
 import { Predmet } from 'src/app/models/predmet.model';
 import { Student } from 'src/app/models/student.model';
+import { LoadingService } from 'src/app/services/loading.service';
 import { PredmetiService } from 'src/app/services/predmeti.service';
 import { UserService } from 'src/app/services/user.service';
 
@@ -18,7 +19,8 @@ export class PredmetPreviewComponent implements OnInit {
   commentBox: string = "";
   private predmetId: number = 0;
 
-  constructor(private router: Router, private predmetiService: PredmetiService, private userService: UserService) {
+  constructor(private router: Router, private predmetiService: PredmetiService, private userService: UserService,
+    public loadingService: LoadingService) {
     this.predmetId = (this.router.getCurrentNavigation()?.extras.state as any).predmetId;
     this.isLoggedIn = localStorage.getItem("authToken");
     if(this.isLoggedIn) {
@@ -38,6 +40,10 @@ export class PredmetPreviewComponent implements OnInit {
     let komentar = await this.predmetiService.addComment(this.userService.user!.id, this.predmetId, this.commentBox);
     console.log(komentar);
     this.predmet?.komentari.push(new Komentar(komentar.id, this.userService.user!, this.predmet, this.commentBox));
+  }
+
+  gotoOglasna() {
+    this.router.navigate(["/oglasna"]);
   }
 
 }
