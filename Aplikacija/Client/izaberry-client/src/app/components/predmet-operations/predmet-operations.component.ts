1e971d0e96fde8b3e839ff0d36188f5e3c096b2a
Urosh <urosh_h@protonmail.com>
2023-06-01 15:31:05 +0200

    predmet operations component update

--- Aplikacija/Client/izaberry-client/src/app/components/predmet-operations/predmet-operations.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/predmet-operations/predmet-operations.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Modul } from 'src/app/models/modul.model';
-import { Predmet } from 'src/app/models/predmet.model';
+import { Predmet, PredmetDto } from 'src/app/models/predmet.model';
 import { Tag } from 'src/app/models/tag.model';
 import { ModuleService } from 'src/app/services/module.service';
 import { PredmetiService } from 'src/app/services/predmeti.service';
@@ -12,7 +12,9 @@ import { TagService } from 'src/app/services/tag.service';
   styleUrls: ['./predmet-operations.component.scss']
 })
 export class PredmetOperationsComponent implements OnInit{
+  predmeti: Predmet[] | null = null;
   predmet: Predmet;
+  selectedPredmet: Predmet | undefined = undefined;
   tagovi: Tag[] | null = null;
   moduli: string[] | null = null;
   semestri: Array<number> = Array(8).fill(0).map((val, idx) => idx+ 1);
@@ -24,6 +26,11 @@ export class PredmetOperationsComponent implements OnInit{
   async ngOnInit(): Promise<void> {
     this.tagovi = await this.tagService.getAllTags();
     this.moduli = await this.modulService.getModuleNames();
+  }
+
+  async getPredmeti() {
+    this.predmeti = await this.predmetService.getAll();
+    this.selectedPredmet = this.predmeti![0];
   }
 
   nazivKeyUp(event: Event){
@@ -54,6 +61,26 @@ export class PredmetOperationsComponent implements OnInit{
   }
 
   async dodajPredmet(){
-    await this.predmetService.create(this.predmet);
+    console.log(this.predmet);
+    console.log(await this.predmetService.create(this.predmet as PredmetDto));
+  }
+
+  cleanPredmeti() {
+    this.predmeti = null;
+    this.selectedPredmet = undefined;
+  }
+
+  predmetChange(event: Event) {
+    let target = event.target as HTMLSelectElement;
+    let naziv = target.options[target.selectedIndex].value;
+    this.selectedPredmet = this.predmeti?.find(p => p.naziv == naziv);
+    console.log(this.selectedPredmet);
+  }
+
+  deletionKeyUp(event: Event) {
+    this.predmet.id = parseInt((event.target as HTMLInputElement).value);
+  }
+  async deletePredmet() {
+    console.log(await this.predmetService.delete(this.predmet.id));
   }
 }
