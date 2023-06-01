0b19bf07ae95ce60b11ae00b1d5fa6c760851cf0
Anja Cokanovic <anja.cokanovic@elfak.rs>
2023-06-01 20:51:14 +0200

    user profile update

--- Aplikacija/Client/izaberry-client/src/app/components/user-profile/user-profile.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/user-profile/user-profile.component.ts
@@ -9,28 +9,50 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.scss']
 })
-export class UserProfileComponent implements OnInit{
+export class UserProfileComponent implements OnInit {
   student: Student | null;
   komentari: Komentar[] | null = null;
+  editingBio: boolean = false;
 
   constructor(private router: Router, private userService: UserService) {
-    //this.student = this.router.getCurrentNavigation()?.extras.state! as Student;
     this.student = null;
   }
+
   async ngOnInit(): Promise<void> {
     let token = localStorage.getItem("authToken");
-    if(token) {
+    if (token) {
       this.student = await this.userService.getUserByToken(token);
       console.log(this.student);
     }
   }
 
+  editBio() {
+    this.editingBio = true;
+  }
+
+  async saveBio() {
+    if (this.student && this.student.bio) {
+      this.student.bio = this.student.bio.trim();
+      this.editingBio = false;
+      try {
+        //await this.userService.updateStudentBio(this.student.id, this.student.bio);
+        console.log("Bio saved successfully");
+      } catch (error) {
+        console.error("Failed to save bio:", error);
+      }
+    }
+  }
+
+  cancelEditBio() {
+    this.editingBio = false;
+  }
+
   showCalendar() {
-     this.router.navigateByUrl("kalendar");
+    this.router.navigateByUrl("kalendar");
   }
 
   showLiteratura() {
-
+    // Implement this method
   }
 
   async showKomentari() {
@@ -44,10 +66,10 @@ export class UserProfileComponent implements OnInit{
     this.router.navigateByUrl("");
   }
 
-   
   redirectToOglasna() {
     this.router.navigateByUrl("oglasna");
   }
+
   redirectToHome() {
     this.router.navigateByUrl("");
   }
