68f8b0de99df87a45f1583408d2a4fe32a08e507
Anja Cokanovic <anja.cokanovic@elfak.rs>
2023-06-01 20:52:47 +0200

    services update

--- Aplikacija/Client/izaberry-client/src/app/services/user.service.ts
+++ Aplikacija/Client/izaberry-client/src/app/services/user.service.ts
@@ -4,6 +4,7 @@ import { Student } from '../models/student.model';
 import { Komentar } from '../models/komentar.model';
 import { environment } from 'src/environments/environment';
 import { firstValueFrom } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,7 +21,43 @@ export class UserService {
     this.user.token = token;
     return this.user;
   }
+ 
 
+  async addNote(tekst: string): Promise<any> {
+    const url = `${environment.backend}/note/dodajNotes/${encodeURIComponent(tekst)}/${this.user?.id}`;
+    const headers = new HttpHeaders({
+      'accept': '*/*',
+      'Authorization': `Bearer ${this.user?.token}`,
+    });
+  
+    try {
+      const response = await this.http.post(url, {}, { headers }).toPromise();
+      console.log(`Note je uspesno dodata`);
+      console.log(response);
+      return response; 
+    } catch (error) {
+      console.error(error); 
+      throw error; 
+    }
+  }
+  
+  async deleteThisNote(noteId: number): Promise<void> {
+    const url = `${environment.backend}/note/obrisiNotes/${noteId}`;
+    const headers = new HttpHeaders({
+      'accept': '*/*',
+      'Authorization': `Bearer ${this.user?.token}`,
+    });
+  
+    try {
+      await this.http.delete(url, { headers }).toPromise();
+      console.log(`Note sa ID ${noteId} je uspesno obrisana`);
+    } catch (error) {
+      console.error(error); 
+      throw error;
+    }
+  }
+  
+  
   async getUserComments(): Promise<Komentar[] | null> {
     let komentari$ = this.http.get<Komentar[]>(`${environment.backend}/komentar/byStudent/${this.user?.id}`);
     let komentari = await firstValueFrom(komentari$) ?? null;
