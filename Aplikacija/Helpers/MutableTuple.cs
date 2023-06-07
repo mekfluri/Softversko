a9bcde69c0481f80fd4ade840653ad9b3fd9d5c8
Urosh <urosh_h@protonmail.com>
2023-06-07 22:38:09 +0200

    Mutable Tuple helper class

---
+++ Aplikacija/Helpers/MutableTuple.cs
@@ -0,0 +1,12 @@
+namespace Helpers;
+
+public class MutableTuple<T1, T2> {
+    public T1 First { get; set; }
+    public T2 Second { get; set; }
+
+    public MutableTuple() {}
+    public MutableTuple(T1 first, T2 second) {
+        First = first;
+        Second = second;
+    }
+}
