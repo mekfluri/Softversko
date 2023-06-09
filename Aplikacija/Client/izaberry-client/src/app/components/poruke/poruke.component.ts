5f20f19d1cec43ec63dfe2824e0c8ba246ab0af0
Jovana <suslic.jovana@elfak.rs>
2023-06-09 20:29:07 +0200

    chat component

--- Aplikacija/Client/izaberry-client/src/app/components/poruke/poruke.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/poruke/poruke.component.ts
@@ -3,6 +3,10 @@ import { HttpClient } from '@angular/common/http';
 import { ActivatedRoute } from '@angular/router';
 import { Poruka } from 'src/app/models/poruke.model';
 import { environment } from 'src/environments/environment';
+import { Student } from 'src/app/models/student.model';
+import { StudentiService } from 'src/app/services/studenti.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { first, firstValueFrom, Observable } from 'rxjs';
 
 @Component({
   selector: 'app-poruke',
@@ -11,32 +15,51 @@ import { environment } from 'src/environments/environment';
 })
 export class PorukeComponent implements OnInit {
   poruke: Poruka[] | null = null;
+  student: Student | null = null;
+  userId: number =0;
+  prikazi:boolean = false;
+  svePoruke: Poruka[] | null = null;
+  text: string = "";
+  chat: number =0;
+  idporuke: number =0;
 
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
+  constructor(private authService: AuthService,private StudentService: StudentiService,private http: HttpClient, private route: ActivatedRoute) {
     const studentId = this.route.snapshot.paramMap.get('studentId');
     if (studentId) {
       const userId = parseInt(studentId);
       console.log("zovem");
-      this.getUserComments(userId);
+      //this.getUserComments(userId);
     }
   }
 
-  ngOnInit() {
-    const studentId = this.route.snapshot.paramMap.get('studentId');
-    if (studentId) {
-      const userId = parseInt(studentId);
-      console.log("zovem");
-      this.getUserComments(userId);
-    }
+  async ngOnInit() {
+    let userId = this.route.snapshot.paramMap.get("userId");
+  if (userId) {
+    this.userId = parseInt(userId);
+  } else {
+    this.userId = this.authService.currentUserId();
+  }
+    
   }
 
-  getUserComments(userId: number) {
+ async getUserComments() {
+    
+
     this.http
-      .get<Poruka[]>(`${environment.backend}/chat/VratiNeprocitanePorukeStudenta/${userId}`)
+      .get<Poruka[]>(`${environment.backend}/chat/VratiNeprocitanePorukeStudenta/${this.userId}`)
       .subscribe(
         (response) => {
-          this.poruke = response;
-          console.log(this.poruke);
+          this.poruke= [];
+          response.forEach(element => {
+              if(element.student.id != this.userId){
+                 this.poruke?.push(element);
+                 console.log(element);
+              }
+              console.log(this.poruke);
+             
+          });
+          
+          
         },
         (error) => {
           console.error('Error fetching user comments:', error);
@@ -44,4 +67,98 @@ export class PorukeComponent implements OnInit {
         }
       );
   }
+
+ async odgovori(poruka: Poruka){
+     this.prikazi = true;
+     this.chat = poruka.chat.id;
+     this.http
+     .get<Poruka[]>(`${environment.backend}/chat/VratiPorukeIzChata/${this.chat!}`)
+     .subscribe(
+       (response) => {
+           this.svePoruke = response;
+           console.log(this.svePoruke);
+
+        
+       },
+       (error) => {
+         console.error('Error fetching user comments:', error);
+         
+       }
+     );
+
+      //dodaj da je poruka procitana (idporuke)
+        
+   await this.http.put(`${environment.backend}/chat/PromeniStatus/${poruka.id}`, {})
+   .subscribe(
+     (response: any) => {
+       console.log(response);
+     
+        
+     },
+     (error) => {
+       console.error('Greška prilikom slanja poruke:', error);
+     }
+   );
+      
+
+ }
+
+  async idKeyUp(event: Event){
+      this.text =  (event.target as HTMLInputElement).value;
+
+  }
+
+  async posalji(){
+
+  await this.http.post(`${environment.backend}/chat/DodajPoruku/${this.userId}/${this.text}`, {})
+   .subscribe(
+     (response: any) => {
+       console.log(response.id);
+       this.idporuke = response.id;
+       console.log(this.idporuke);
+        
+     },
+     (error) => {
+       console.error('Greška prilikom slanja poruke:', error);
+     }
+   );
+ 
+   console.log(this.idporuke); 
+
+  }
+
+  async sacuvaj()
+  {
+    
+   await this.http.put(`${environment.backend}/chat/DodajPorukuUChat/${this.idporuke}/${this.chat}`, {})
+   .subscribe(
+     (response: any) => {
+       console.log(response);
+     
+        
+     },
+     (error) => {
+       console.error('Greška prilikom slanja poruke:', error);
+     }
+   );
+  
+  }
+
+  async obrisi(poruka: Poruka){
+    await this.http.delete(`${environment.backend}/chat/ObrisiPoruku/${poruka.id}`, {})
+    .subscribe(
+      (response: any) => {
+        console.log(response);
+      
+         
+      },
+      (error) => {
+        console.error('Greška prilikom slanja poruke:', error);
+      }
+    ); 
+  }
+
+
 }
+
+
