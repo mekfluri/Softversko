0b766ba02c1236e0c0df5cb2e17184fa29d38525
Urosh <urosh_h@protonmail.com>
2023-06-10 22:01:40 +0200

    mentor operations component update

--- Aplikacija/Client/izaberry-client/src/app/components/mentor-operations/mentor-operations.component.ts
+++ Aplikacija/Client/izaberry-client/src/app/components/mentor-operations/mentor-operations.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MentorRequestPreview } from 'src/app/models/mentor-req-backend.model';
+import { MessageResponse } from 'src/app/models/response.model';
 import { MentorService } from 'src/app/services/mentor.service';
 
 @Component({
@@ -10,6 +11,7 @@ import { MentorService } from 'src/app/services/mentor.service';
 })
 export class MentorOperationsComponent implements OnInit{
   zahtevi: MentorRequestPreview[] | null = null;
+  response: MessageResponse = new MessageResponse();
   constructor(private mentorService: MentorService, private router: Router){}
 
   async ngOnInit(): Promise<void> {
@@ -17,15 +19,43 @@ export class MentorOperationsComponent implements OnInit{
     console.log(this.zahtevi);
   }
 
+  private removeZahtev(id: number) {
+    let zahtev = this.zahtevi?.find(zahtev => zahtev.id == id);
+    let idx = this.zahtevi?.indexOf(zahtev!);
+    this.zahtevi?.splice(idx!, 1);
+  }
+
   studentProfile(ev: Event){ 
     this.router.navigate(["profile", (ev.target as HTMLParagraphElement).id]);
   }
 
   async odobriMentorstvo(ev: Event) {
-    await this.mentorService.acceptRequest(parseInt((ev.target as HTMLButtonElement).id));
+    try {
+      let zahtevId = parseInt((ev.target as HTMLButtonElement).id);
+      await this.mentorService.acceptRequest(zahtevId);
+      this.response.message = "Uspesno odobreno mentorstvo!";
+      this.response.showResponse();
+      this.removeZahtev(zahtevId);
+    }
+    catch(err: any){
+      this.response.isError = true;
+      this.response.message = "Neuspesno odobrenje mentrostva!";
+      this.response.showResponse();
+    }
   }
 
   async odbijMentorstvo(ev: Event) {
-    await this.mentorService.deleteRequest(parseInt((ev.target as HTMLButtonElement).id));
+    try {
+      let zahtevId = parseInt((ev.target as HTMLButtonElement).id);
+      await this.mentorService.deleteRequest(zahtevId);
+      this.response.message = "Uspesno odbijeno mentorstvo!";
+      this.response.showResponse();
+      this.removeZahtev(zahtevId);
+    }
+    catch(err: any){
+      this.response.isError = true;
+      this.response.message = "Neuspesno odbijeno mentorstvo!";
+      this.response.showResponse();
+    }
   }
 }
